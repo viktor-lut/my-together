7bbae5a1e135860ca8f194c9bc0e1d351cdc4020
Viktor Bogutskii <info@siteograf.com>
2019-04-04 16:36:39 -0700

    message to info controller (#41)
    ok

--- modules/info/infoControllers.js
+++ modules/info/infoControllers.js
@@ -1,7 +1,9 @@
+const message = require('./../core/message');
+
 const info = (req, res) => {
   const now = new Date();
 
-  const message = {
+  const answer = {
     timestamp: now.getTime(),
     localeString: now.toLocaleString(),
     getTimezoneOffset: now.getTimezoneOffset(),
@@ -9,7 +11,7 @@ const info = (req, res) => {
     name: 'Together app v0.1.0',
   };
 
-  res.status(200).json(message);
+  res.status(200).json(message.success(answer));
 };
 
 module.exports = info;
