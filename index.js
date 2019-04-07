bca96bb969fb613301515bcb1fdbd87d35fbbc60
Yulia Tuzov <pachkovska@gmail.com>
2019-04-07 16:05:08 -0700

    Added basic CORS handling. (#51)

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const logger = require('./modules/core/logger');
 const routes = require('./modules/core/routes');
 const errorHandling = require('./modules/core/errorHandling');
 const ignoreFavicon = require('./modules/core/ignoreFavicon');
+const cors = require('./modules/core/cors');
 
 const PORT = +process.env.PORT || 5000;
 const app = express();
@@ -11,6 +12,7 @@ logger(app);
 routes(app);
 ignoreFavicon(app);
 errorHandling(app);
+cors(app);
 
 app.listen(PORT, () => {
   console.log(`Node cluster worker ${process.pid}: listening on port ${PORT}`);
---
+++ modules/core/cors.js
@@ -0,0 +1,14 @@
+function cors(app) {
+    app.use((req, res, next) => {
+       res.header('Access-Control-Allow-Origin', '*');
+       res.header('Access-Control-Allow-Headers',
+           'Origin, Content-Type, Accept, Authorization');
+        if (req.method === 'OPTIONS') {
+            res.header('Access-Control-Allow-Methods', 'GET, POST, PATCH, DELETE');
+            return res.status(200).json({});
+        }
+        next();
+    });
+}
+
+module.exports = cors;
