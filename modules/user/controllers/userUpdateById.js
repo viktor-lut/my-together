513a0f7df49b80aa4c1a3f43e713732d47a4daaa
Julia <47610718+jstepiko@users.noreply.github.com>
2019-04-09 17:26:38 -0700

    TOG-18 Stepiko/user update by Id (#77)
    Added functionality for userUpdateById controller

--- modules/user/controllers/userUpdateById.js
+++ modules/user/controllers/userUpdateById.js
@@ -1,5 +1,20 @@
 const message = require('../../core/message');
+const User = require('./../userModel');
 
-const userUpdateById = (req, res) => res.status(200).json(message.success('Changes details', req.params.userId));
+const userUpdateById = (req, res) => {
+  const id = req.params.userId;
+  User.updateOne({ _id: id }, { $set: req.body })
+    .exec()
+    .then(doc => {
+      if (doc.n) {
+        res.status(200).json(message.success('User updated successfully'));
+      } else {
+        res.status(400).json(message.fail('User not found'));
+      }
+    })
+    .catch(err => {
+      res.status(500).json(message.fail(err));
+    });
+};
 
 module.exports = userUpdateById;
