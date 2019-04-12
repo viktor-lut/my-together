5264ac389d595ec496147030cb36c4d39ec1cee5
IgorRadzyvil <48041143+IgorRadzyvil@users.noreply.github.com>
2019-04-12 10:43:43 +0300

    TOG-12 Added new functionality for controller userGetById (#86)
    * Added new functionality for controller userGetById
    
    * Added new functionality for controller userGetById

--- modules/user/controllers/userGetById.js
+++ modules/user/controllers/userGetById.js
@@ -1,5 +1,21 @@
 const message = require('../../core/message');
+const User= require('./../userModel');
 
-const userGetById = (req, res) => res.status(200).json(message.success('User Get By Id', req.params.userId));
+const userGetById = (req, res) => {
+  const id = req.params.userId;
+  User.findById(id)
+    .select()
+    .exec()
+    .then(doc => {
+      if (doc) {
+        res.status(200).json(message.success('User Get By Id', doc));
+      } else {
+        res.status(400).json(message.success('No found user for provided id'));
+      }
+    })
+    .catch(err => {
+      res.status(500).json(message.fail('Get User Error', err));
+    });
+};
 
 module.exports = userGetById;
