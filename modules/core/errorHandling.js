8dc45565875594b9191997dc805ccfb0e081528d
Viktor Bogutskii <info@siteograf.com>
2019-04-15 16:25:01 -0700

    impr (#98)

--- modules/core/errorHandling.js
+++ modules/core/errorHandling.js
@@ -1,10 +1,9 @@
 const message = require('./message');
 
 function errorHandling(app) {
-  app.use((req, res, next) => res.status(404).json(message.fail('API not found'))); // eslint-disable-line no-unused-vars
+  app.use((req, res) => res.status(404).json(message.fail('API not found')));
 
-  app.use((error, req, res, next) => {
-    // eslint-disable-line no-unused-vars
+  app.use((error, req, res) => {
     res.status(error.status || 500);
     res.json(message.fail(error.message));
   });
--- modules/core/mongoConnection.js
+++ modules/core/mongoConnection.js
@@ -10,14 +10,13 @@ const options = {
 
 function mongoConnection() {
   mongoose
-    .connect(
-      connectionString,
-      options,
-    )
+    .connect(connectionString, options)
+    // eslint-disable-next-line no-console
     .catch(err => console.log(err));
 
   // When the connection is disconnected
   mongoose.connection.on('disconnected', () => {
+    // eslint-disable-next-line no-console
     console.log('Mongoose default connection disconnected');
   });
 }
