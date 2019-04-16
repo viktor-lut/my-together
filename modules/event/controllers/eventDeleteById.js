c5b452e5aeef6e5458b79347e510de6178ee2c8d
Sergey Miksyuk <miksyuk.sergey@gmail.com>
2019-04-16 07:45:45 +0300

    TOG-17 Miksyuk/added functionality eventDeleteById (#93)
    * add functionality to eventGetAll
    
    * add functionality to eventDeleteById
    
    * add requests for eventCreate and eventGetAll and DeleteById
    
    * fix with prettier

--- modules/event/controllers/eventDeleteById.js
+++ modules/event/controllers/eventDeleteById.js
@@ -1,7 +1,21 @@
 const message = require('../../core/message');
+const Event = require('../eventModel');
 
 const eventDeleteById = (req, res) => {
-  res.status(200).json(message.success('eventDeleteById', req.params.eventId));
+  const id = req.params.eventId;
+
+  Event.remove({_id: id})
+    .exec()
+    .then(doc => {
+      if (doc.n) {
+        res.status(200).json(message.success('Event deleted successfully', id));
+      } else {
+        res.status(400).json(message.fail('Event not found', id));
+      }
+    })
+    .catch(err => {
+      res.status(500).json(message.fail('Delete event error', err));
+    });
 };
 
 module.exports = eventDeleteById;
--- modules/event/controllers/eventGetAll.js
+++ modules/event/controllers/eventGetAll.js
@@ -1,7 +1,17 @@
 const message = require('../../core/message');
+const Event = require('../eventModel');
 
 const eventGetAll = (req, res) => {
-  res.status(200).json(message.success('eventGetAll'));
+  Event.find()
+    .sort({createdAt: -1})
+    .select('-__v')
+    .exec()
+    .then(docs => {
+      res.status(200).json(message.success('Events are showed', docs));
+    })
+    .catch(err => {
+      res.status(500).json(message.fail('Get all events error', err));
+    });
 };
 
 module.exports = eventGetAll;
